f5fafb230e57cbcf0f6302ada3f086a41987d042
steven.rua <steven.rua@globalbank.com.pa>
2024-08-04 11:55:09 -0500

    agrego variables de conexión AWS

--- providers.tf
+++ providers.tf
@@ -13,6 +13,8 @@ terraform {
 }
 
 provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
   region = "us-east-1"
   default_tags {
     tags = var.tags
--- variables.tf
+++ variables.tf
@@ -33,3 +33,7 @@ variable "ingress_port_list" {
   description = "Lista de puertos de ingress"
   type = list(number)
 }
+
+variable "access_key" {}
+
+variable "secret_key" {}
